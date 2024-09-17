46a7afc903a50db1a0aca2373925de715d9f0ca8
nirrusso <127675955+nirrusso@users.noreply.github.com>
2024-09-17 08:32:57 -0300

    Delete soso/CADCOMPONENTE directory

--- soso/CADCOMPONENTE/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$banco = "bdcomponente";
-$conexao = mysqli_connect($host, $user, $pass) or die (mysqli_error($conexao));
-mysqli_select_db($conexao, $banco);
-?>
